a5580422752bd474a9376b5cc18cca76fc8cccc9
Ohkubo KOHEI <ohkubo@magician.jp>
2016-01-02 20:34:54 +0900

    AR4

--- lib/wordpress.rb
+++ lib/wordpress.rb
@@ -1,139 +1,124 @@
-gem 'activerecord', '~> 3.0'
+gem 'activerecord', '~> 4.0'
 require 'active_record'
 
 # Adapted from http://snippets.dzone.com/posts/show/1314 and
 # considerably extended
 
-class ActiveRecord::Wordpress < ActiveRecord::Base
-  self.abstract_class = true
-end
+module Wordpress
+  class Base < ActiveRecord::Base
+    self.abstract_class = true
+    self.table_name_prefix = 'wp_'
+  end
 
-class WpBlogComment < ActiveRecord::Wordpress
+  class Comment < Base
+    self.primary_key = "comment_ID"
 
-  # if wordpress tables live in a different database (i.e. 'wordpress') change the following
-  # line to set_table_name "wordpress.wp_comments"
-  # don't forget to give the db user permissions to access the wordpress db
-  set_table_name "wp_comments"
-  set_primary_key "comment_ID"
+    belongs_to :post, foreign_key: "comment_post_ID"
 
-  belongs_to :post , :class_name => "WpBlogPost", :foreign_key => "comment_post_ID"
+    validates_presence_of :comment_post_ID, :comment_author, :comment_content, :comment_author_email
+    validate :valid_comment_status
 
-  validates_presence_of :comment_post_ID, :comment_author, :comment_content, :comment_author_email
-
-  def validate_on_create
-    if WpBlogPost.find(comment_post_ID).comment_status != 'open'
-      errors.add_to_base('Sorry, comments are closed for this post')
+    def valid_comment_status
+      unless post.comment_status == 'open'
+        errors.add_to_base('Sorry, comments are closed for this post')
+      end
     end
   end
 
-end
-
-class WpPostMeta < ActiveRecord::Wordpress
-  set_table_name "wp_postmeta"
-  set_primary_key "meta_id"
-  belongs_to :blog_post, :foreign_key => :post_id, :class_name => 'WpBlogPost'
-end
-
-class WpBlogPost < ActiveRecord::Wordpress
-
-  set_table_name "wp_posts"
-  set_primary_key "ID"
-
-  has_many :comments, :class_name => "WpBlogComment", :foreign_key => "comment_post_ID"
-  has_many :metas, :class_name => 'WpPostMeta', :foreign_key => 'post_id'
-
-  has_many :term_relationships, :class_name => 'WpTermRelationship', :foreign_key => 'object_id'
-  has_many :term_taxonomies, :through => :term_relationships
-  has_many :taggings, :through => :term_relationships, :source => :term_taxonomy, :conditions => ['wp_term_taxonomy.taxonomy = ?', 'post_tag']
-
-  belongs_to :author, :class_name => 'WpUser', :foreign_key => 'post_author'
-
-  scope :published, :conditions => {:post_status => 'publish'}
-  default_scope :order => 'post_date DESC'
-
-  validates_presence_of :post_modified, :post_modified_gmt, :post_date, :post_date_gmt
-  before_validation :set_timestamps
-  
-  def set_timestamps
-    self.post_modified = DateTime.now
-    self.post_modified_gmt = self.post_modified.utc
-    self.post_date ||= self.post_modified
-    self.post_date_gmt ||= self.post_date.utc
+  class PostMeta < Base
+    self.table_name = "#{table_name_prefix}postmeta"
+    self.primary_key = "meta_id"
+    belongs_to :post
   end
-  
-  def published?
-    self.status == 'published' and self.post_date <= DateTime.now
-  end
-  
-  def tags
-    taggings.collect(&:term)
-  end
- 
-  def self.find_by_permalink(year, month, day, title)
-    first(:conditions => ["YEAR(post_date) = ? AND MONTH(post_date) = ? AND DAYOFMONTH(post_date) = ? AND post_name = ?",
-      year.to_i, month.to_i, day.to_i, title])
-  end
-end
 
-class WpTerm < ActiveRecord::Wordpress
-  set_table_name 'wp_terms'
-  set_primary_key 'term_id'
-  has_many :taxonomies, :class_name => 'WpTermTaxonomy', :foreign_key => 'term_id'
-  has_many :relationships, :through => :taxonomies
- 
-  def posts
-    relationships.collect(&:post)
-  end
-end
+  class Post < Base
+    has_many :comments, foreign_key: "comment_post_ID"
+    has_many :metas, :class_name => 'PostMeta'
 
-class WpTermTaxonomy < ActiveRecord::Wordpress
-  set_table_name "wp_term_taxonomy"
-  set_primary_key 'term_taxonomy_id'
-  belongs_to :term, :class_name => 'WpTerm'
-  has_many :relationships, :class_name => 'WpTermRelationship', :foreign_key => 'term_taxonomy_id'
-end
+    has_many :term_relationships, :foreign_key => 'object_id'
+    has_many :term_taxonomies, :through => :term_relationships
+    has_many :taggings, through: :term_relationships, :source => :term_taxonomy#, :conditions => ['#{table_name_prefix}term_taxonomy.taxonomy = ?', 'post_tag']
 
-class WpTermRelationship < ActiveRecord::Wordpress
-  set_table_name "wp_term_relationships"
-  belongs_to :post, :class_name => 'WpBlogPost', :foreign_key => 'object_id'
-  belongs_to :term_taxonomy, :class_name => 'WpTermTaxonomy'
-end
+    belongs_to :author, :class_name => 'User', :foreign_key => 'post_author'
 
-class WpLink < ActiveRecord::Wordpress
-  set_primary_key 'link_id'
-end
+    scope :published, ->{ where(post_status: 'publish') }
+    default_scope { order('post_date DESC') }
 
-class WpUser < ActiveRecord::Wordpress
-  set_primary_key 'ID'
-  has_many :meta_details, :class_name => 'WpUserMeta', :foreign_key => 'user_id'
-  has_many :blog_posts, :class_name => 'WpBlogPost', :foreign_key => 'post_author'
-    
-  before_validation :store_registration_time, :on => :create
-  
-  validates_presence_of :user_registered
-  validates_uniqueness_of :user_email, :user_login
+    validates_presence_of :post_modified, :post_modified_gmt, :post_date, :post_date_gmt
+    before_validation :set_timestamps
 
-  def store_registration_time
-    self.user_registered ||= DateTime.now
-  end
-  
-  class <<self
-    # Note: Recent versions of WP do not simply store an MD5
-    # they use PHPPass (wp-includes/class-phpass.php)
-    # If anyone has written a ruby equivalent, please let me know so I 
-    # can integrate it
-    def encrypt(password)
-      Digest::MD5.hexdigest(password)
+    def set_timestamps
+      self.post_modified = DateTime.now
+      self.post_modified_gmt = self.post_modified.utc
+      self.post_date ||= self.post_modified
+      self.post_date_gmt ||= self.post_date.utc
+    end
+
+    def published?
+      status == 'published' and post_date <= DateTime.now
+    end
+
+    def tags
+      taggings.pluck(:term)
     end
   end
-end
 
-class WpUserMeta < ActiveRecord::Wordpress
-  set_table_name 'wp_usermeta'
-  set_primary_key 'umeta_id'
-  belongs_to :user, :class_name => 'WpUser'
-end
+  class Term < Base
+    self.primary_key = 'term_id'
+    has_many :taxonomies, :class_name => 'TermTaxonomy'
+    has_many :relationships, :through => :taxonomies
 
-class WpOption < ActiveRecord::Wordpress
-  set_primary_key 'option_id'
+    def posts
+      relationships.includes(:post).map(&:post)
+    end
+  end
+
+  class TermTaxonomy < Base
+    self.table_name = "#{table_name_prefix}term_taxonomy"
+    self.primary_key = 'term_taxonomy_id'
+    belongs_to :term
+    has_many :relationships, :class_name => 'TermRelationship'
+  end
+
+  class TermRelationship < Base
+    belongs_to :post, foreign_key: 'object_id'
+    belongs_to :term_taxonomy
+  end
+
+  class Link < Base
+    self.primary_key = 'link_id'
+  end
+
+  class User < Base
+#    self.primary_key = 'ID'
+    has_many :user_metas
+    has_many :posts, foreign_key: 'post_author'
+
+    validates_presence_of :user_registered
+    validates_uniqueness_of :user_email, :user_login
+
+    before_validation on: :create do
+      self.user_registered ||= DateTime.now
+    end
+
+    class << self
+      # Note: Recent versions of WP do not simply store an MD5
+      # they use PHPPass (wp-includes/class-phpass.php)
+      # If anyone has written a ruby equivalent, please let me know so I
+      # can integrate it
+      def encrypt(password)
+        Digest::MD5.hexdigest(password)
+      end
+    end
+  end
+
+  class UserMeta < Base
+    self.table_name = "#{table_name_prefix}usermeta"
+    self.primary_key = 'umeta_id'
+    belongs_to :user
+  end
+
+  class Option < Base
+    self.primary_key = 'option_id'
+  end
 end
